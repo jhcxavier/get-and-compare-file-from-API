d231eb663c96bd4b67a963c92e2d5cefa8808d46
João Henrique Xavier <jhcxavier@gmail.com>
2019-10-07 16:40:34 -0400

    updating readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # get-and-compare-file-from-API
+ This project was initiated with the goal of completing a task in my company.
+
+ The goal was fetch data from our API, make it JSON and save in a file.
+
+ This algorithm can be used to fetch as many entrypoint as it need. 
+
+ For multiple entry points you can use the sample object in line 3 to specify, id, type, name...
+
+ In the const endpoints is where you will specify your enpoints.
+
+ The file testapi is where you will declare your api url, also the of your files to be saved.
--- apitest5.js
+++ apitest5.js
@@ -23,52 +23,8 @@ const sample = {
 const endpoints = [
     `/apps/${sample.name}/catalog/${sample.catalog_id}`,
     `/apps/states/${sample.catalog_id}`,
-    `/apps/feed/processed/${sample.catalog_id}`,
-    //`/catalogs`,
-    `/catalogs/type/${sample.type_name}`,
-    `/catalogs/${sample.catalog_id}`,
-    `/catalogs/${sample.catalog_id}/active`,
-    `/catalogs/${sample.catalog_id}/Category/${sample.category_id}`,
-    // `/catalogs/${sample.catalog_id}/products`,
-    `/catalogs/${sample.catalog_id}/search/${sample.search_key}`,
-    `/catalogs/${sample.catalog_id}/searching/${sample.search_key}`,
-    `/catalogs/${sample.catalog_id}/pdf`,
-    // `/catalogs/${sample.catalog_id}/categories/${sample.category_id}/products`,
-    `/catalogs/${sample.id}/categories`,
-    `/catalogs/${sample.id}/categories/${sample.category_id}`,
-    `/categories/${sample.id}`,
-    `/catalogs/${sample.id}/Category/${sample.category_id}/categories`,
-    `/catalogs/${sample.id}/Category/${sample.category_id}/categories/${sample.category_id}`,
-    `/catalogs/${sample.id}/grid-categories`,
-    `/catalogs/${sample.id}/savingsoffer`,
-    `/catalogs/${sample.catalog_id}/user_options`,
-    `/catalogs/${sample.catalog_id}/versions`,
-    `/catalogs/${sample.catalog_id}/products/${sample.product_id}`,
-    //  `/contacts`,
-    `/contacts/${sample.contact_id}`,
-    `/pdf/annotations/${sample.pdf_catalog_page_id}`,
-    `/pdf/annotations/${sample.pdf_catalog_page_id}/product/${sample.product_id}`,
-    //  `/pdf/annotations/${sample.pdf_catalog_page_id}/products`,
-    `/pdf/annotation/${sample.pdf_annotation_id}`,
-    `/pdf/${sample.catalog_id}/page-options/${sample.page_id}`,
-    //   `/departments`,
-    `/departments/${sample.department_id}/catalogs`,
-    //      `/catalogs/${sample.catalog_id}/dynamic`,
-    //     `/catalogs/${sample.catalog_id}/dynamic_pdf`,
-    //      `/catalogs/${sample.catalog_id}/dynamic_mobile_pdf`,
-    //   `/catalogs/${sample.catalog_id}/dynamic_mobile`,
-    `/catalogs/${sample.catalog_id}/category/${sample.category_id}`,
-    `/catalogs/${sample.catalog_id}/mobile_category/${sample.category_id}`,
-    `/catalogs/${sample.catalog_id}/dynamic/single`,
-    //    `/dyncatalogs/${sample.catalog_id}`,
-    //    `/dyncatalogs/${sample.catalog_id}/orphaned`,
-    //  `/dyncatalogs/${sample.catalog_id}/products`,
-    `/dyncatalogs/${sample.catalog_id}/products/${sample.product_id}`,
-    `/dyncatalogs/${sample.catalog_id}/search/${sample.search_key}`,
-    `/dyncatalogs/${sample.catalog_id}/searching/${sample.search_key}`,
-    `/dyncatalogs/${sample.catalog_id}/lifestyle`,
-    `/dyncatalogs/${sample.catalog_id}/page/${sample.page_id}`,
-
+    `/apps/feed/processed/${sample.catalog_id}`
+   
 ];
 
 
@@ -83,7 +39,6 @@ module.exports.test = function (api, filepath) {
     var a;
     endpoints.forEach(async endpoint => {
 
-        // endpoint= String.replace(endpoint, ":catalog_id", catalog_id);
         arr[endpoint] = "";
         var apicall = api + endpoint + '?limit=1';
         promises.push(fetch(apicall).then(function (data) {
@@ -92,7 +47,6 @@ module.exports.test = function (api, filepath) {
             });
         })
             .then(function (json) {
-                //        console.log("***** ", json);
                 arr[endpoint] = json;
             }));
     });
